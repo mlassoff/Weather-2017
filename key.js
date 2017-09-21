f029278be2b400ad88049191fcf0f072302ad9e6
Mark Lassoff <mark@learntoprogram.tv>
2017-09-20 21:23:28 -0400

    Complete Weather Underground App
    Here's the complete weather underground API app that was demonstrated during Hack Night.

--- key.js
+++ key.js
@@ -1,6 +1,6 @@
 var marksKey = "df813c985b3d8af6";
 /*
 
-http://api.wunderground.com/api/df813c985b3d8af6/geolookup/conditions/q/CT/New_Haven.json
+http://api.wunderground.com/api/df813c985b3d8af6/geolookup/conditions/q/06510.json
 
 */
